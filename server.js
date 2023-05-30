72c3d1f695a7bdf3432a222b0ef349495b4e611c
vworo <victorworobec@gmail.com>
2023-05-31 00:16:23 +1000

    First commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// Import required modules
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+// Create an express app instance
+const app = express();
+
+// Create an HTTP server using the Express app instance
+const server = http.createServer(app);
+
+// Create a WebSocket server instance and attach it to the HTTP server
+const webSocketServer = new WebSocket.Server({ server });
+
+// Start the server listening on port 3000
+server.listen(3000, () => {
+    console.log("Websocket server started on port 3000");
+});
+
+// Listen for WebSocket connections
+webSocketServer.on('connection', (socket) => {
+    // Log a message when a new client connects
+    console.log('client connected.');
+
+    // Listen for incoming WebSocket messages
+    socket.on('message', (data) => {
+        // Broadcast the message to all connected clients 
+        webSocketServer.clients.forEach(function each(client) {
+            if (client !== socket && client.readyState === WebSocket.OPEN) {
+                client.send(data.toString());
+                console.log("message", data.toString());
+            };
+        });
+    });
+
+    // Listen for WebSocket connection close events
+    socket.on('close', () => {
+        // Log a message when a client disconnects
+        console.log('Client disconnected');
+    });
+});
